b7e55449320c95303945c6a5b040f8666eb448d0
DavidFlorin <florin.david@bitsoftware.ro>
2014-07-24 00:34:15 +0300

    start

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 /**
  * 
  */
-debug = true;
+function log(msg) {
+	console.log(msg);
+};
 
 // Thanks @unscriptable - http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
 var debounce = function (func, threshold, execAsap) {
@@ -21,21 +23,23 @@ var debounce = function (func, threshold, execAsap) {
 
 		timeout = setTimeout(delayed, threshold || 100);
 	};
-}
-
-var getU = function() {
-	console.log("Get users!!!!!!!!!");
-	return "User Tst - !123!!!!!!!!!";
-}
+};
 
 //Entry Point - start changes listener
 var recalcTotalsObserver = new MutationObserver(function(mutations)
 {	
-	//if (debug)
-	//	console.log("");
-	//recalculate();
-	debounce(getU, 500, true);
+	$.each(mutations, function(index, mutation)
+	{
+		var $target = $(mutation.target);
+		if ($target.hasClass('list-card-title'))
+		{
+			log("Cart title changed: " + $target[0].innerText);
+			log($target.closest('.list-card')[0].listCard);
+		}
+	});
+	
 });
+
 recalcTotalsObserver.observe(document.body, {childList: true, characterData: true, attributes: false, subtree: true});
 
 //var recalculate = debounce(getUsers, 500, true);
@@ -43,7 +47,12 @@ recalcTotalsObserver.observe(document.body, {childList: true, characterData: tru
 function getUsers() {
 	console.log("Get users");
 	return "User Tst - !123";
-}
+};
+
+var getU = function() {
+	console.log("Get users!!!!!!!!!");
+	return "User Tst - !123!!!!!!!!!";
+};
 
 
 
